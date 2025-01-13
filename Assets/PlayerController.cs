47e28b3695b6afee5a3b78894f4e49d82b23987a
Connor Jones <115162197+ConmanConnor@users.noreply.github.com>
2025-01-13 21:58:34 +0000

    comments
    comments comments comments

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -65,8 +65,10 @@ public class PlayerController : MonoBehaviour
 
    private void Jump()
     {
+        //checks if player is grounded
         if(isGrounded)
         {
+            //Adds upward force
             rb.AddForce(Vector3.up * jumpForce * 10f, ForceMode.Impulse);
         }
         
@@ -75,41 +77,55 @@ public class PlayerController : MonoBehaviour
 
     private void MovePerformed(InputAction.CallbackContext context)
     {
+        //Reads player input as vector 2
         InputMove = context.ReadValue<Vector2>();
+        //if coroutine is null and player is not moving 
         if(movePlayer == null && !isMoving)
         {
+            //Player is now moving
             isMoving = true;
+            //Start the coroutine to move player
             movePlayer = StartCoroutine(Move());
         }
     }
 
     private void MoveCancelled(InputAction.CallbackContext context)
     {
+        //Reads player input as vector 2
         InputMove = context.ReadValue<Vector2>();
+        //if coroutine does not = null or if player stops input
         if (movePlayer != null)
         {
+            //player not moving
             isMoving = false;
+            //stop coroutine
             StopCoroutine(Move());
+            //Couroutine is now null 
             movePlayer = null;
         }
     }
     IEnumerator  Move()
     {
+        //Calculates movement directions using vector 3 and input values
         Vector3 movedirection = playerHead.forward * InputMove.y + playerHead.right * InputMove.x;
+        //Adds force to player
         rb.AddForce(movedirection.normalized * moveSpeed * 10f, ForceMode.Force);
         yield return new WaitForFixedUpdate();
     }
 
     private void groundCheck()
     {
+        //Draws ray 
         if(Physics.Raycast(playerFeet.transform.position, transform.TransformDirection(Vector3.down),out hit, 0.5f,layerMaskGround))
         {
+            //player is grounded
             isGrounded = true;
             Debug.Log("Ground");
             Debug.DrawRay(playerFeet.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.green);
         }
         else
         {
+            //Player is not grounded
             isGrounded = false;
             Debug.Log("No Ground");
             Debug.DrawRay(playerFeet.transform.position, transform.TransformDirection(Vector3.down) * hit.distance, Color.red);
